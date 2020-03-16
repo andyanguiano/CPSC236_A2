bb62714b37b01fa76749f3de386ca97d654857c7
Andy Anguiano <aanguiano@chapman.edu>
2020-03-16 07:13:22 +0000

    videos done

--- Assets/Scripts/bullet_controller.cs
+++ Assets/Scripts/bullet_controller.cs
@@ -1,23 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class bullet_controller : MonoBehaviour
 {
     public float speed;
     private Rigidbody2D rb;
+
+    private Text scoreText;
+
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         rb.velocity = new Vector2(0, speed);
+        scoreText = GameObject.Find("ScoreText").GetComponent<Text>();
     }
 
     // Update is called once per frame
     void Update()
     {
         if (Camera.main.WorldToViewportPoint(transform.position).y > 1)
+        {
+            scoreText.GetComponent<score_control>().score -= 5;
+            scoreText.GetComponent<score_control>().UpdateScore();
+
             Destroy(this.gameObject);
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -26,6 +36,10 @@ public class bullet_controller : MonoBehaviour
         {
             GameObject.Destroy(this.gameObject);
             GameObject.Destroy(collision.gameObject);
+
+            scoreText.GetComponent<score_control>().score += 10;
+            scoreText.GetComponent<score_control>().UpdateScore();
+
         }
     }
 }
