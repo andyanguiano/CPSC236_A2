2ca53c0f333c5f7dde1d1b73a48554b0b89d5b11
Andy Anguiano <aanguiano@chapman.edu>
2020-03-16 05:33:42 +0000

    barrier sprite not showing but functionality works

--- Assets/Scripts/Enemy_manager.cs
+++ Assets/Scripts/Enemy_manager.cs
@@ -8,11 +8,14 @@ public class Enemy_manager : MonoBehaviour
     private float maxTimer;
     public GameObject enemy;
 
+    public float timerMin = 5f;
+    public float timerMax = 12f;
+
     // Start is called before the first frame update
     void Start()
     {
         timer = 0;
-        maxTimer = Random.Range(5f, 12f);
+        maxTimer = Random.Range(timerMin, timerMax);
     }
 
     // Update is called once per frame
@@ -26,18 +29,26 @@ public class Enemy_manager : MonoBehaviour
         float y = 1.25f;
         Vector3 spawnPoint = Camera.main.ViewportToWorldPoint(new Vector3(Random.Range(0, 1f), y, 0));
         spawnPoint.z = 0;
+
+        //adjust x-axis positition
+        float dist = (this.transform.position - Camera.main.transform.position).z;
+        float leftBorder = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, dist)).x;
+        float rightBorder = Camera.main.ViewportToWorldPoint(new Vector3(1, 0, dist)).x;
+        Vector3 enemySize = enemy.GetComponent<Renderer>().bounds.size;
+        spawnPoint.x = Mathf.Clamp(spawnPoint.x, leftBorder + enemySize.x / 2, rightBorder + enemySize.x / 2);
+
         GameObject.Instantiate(enemy, spawnPoint, new Quaternion(0, 0, 0, 0));
 
     }
 
-    IEnumerable SpawnEnemyTimer()
+    IEnumerator SpawnEnemyTimer()
     {
         if(timer >= maxTimer)
         {
             //Spawn an enemy
             SpawnEnemy();
             timer = 0;
-            maxTimer = Random.Range(5f, 12f);
+            maxTimer = Random.Range(timerMin, timerMax);
         }
         timer += 0.1f;
         yield return new WaitForSeconds(0.1f);
